74b29d00e15446b3f2e2c1b659a1862beb3cadf2
blaverick62 <blav62@gmail.com>
2016-09-01 11:05:50 -0600

    Kippo project added, crude import statement added.

---
+++ kippo_test.py
@@ -0,0 +1,15 @@
+############################################################
+# NAME: C1C Braden J Laverick                              #
+# PROJECT: HIVE Project                                    #
+# FILE: Kippo Test File - kippo_test.py                    #
+# DESCRIPTION:                                             #
+# This file is a test run of the kippo API, it will be     #
+# integrated into the HIVE Project after testing is        #
+# complete.                                                #
+#########################################################
+
+from copy import deepcopy, copy
+import os
+
+from kippo.kippo.core import honeypot
+
