b802431649525b93d784496925629073cdeec668
blaverick62 <blav62@gmail.com>
2016-11-01 15:34:24 -0600

    Added detonation chamber IP input

--- SIREN_CTRL.py
+++ SIREN_CTRL.py
@@ -14,6 +14,7 @@ import threading
 from server.ftp_server import *
 from server.http_server import *
 from server.telnet_server import *
+from detonation import detChamber
 
 import subprocess, sys, os, socket
 
@@ -32,6 +33,28 @@ def knode_stop():
     os.chdir('..')
 
 def main():
+    dets = -1
+
+    while (dets < 0):
+        dets = input("How many detonation chambers would you like to connect? >> ")
+
+    linList = []
+    winList = []
+
+    if (dets > 0):
+        for i in range(dets):
+            detaddr = input("What is the IP Address of this chamber? >>")
+            dettype = input("What type is this detonation chamber? Y for Linux/OSX or N for Windows >>").upper()
+
+            if (dettype == 'Y'):
+                dettype = True
+            else:
+                dettype = False
+
+            if (dettype == True):
+                linList.append(detaddr)
+            else:
+                winList.append(detaddr)
 
     # http_thread = http_ctrl()
     # http_thread.setDaemon(True)
