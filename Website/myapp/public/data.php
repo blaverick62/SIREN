9e5afb28629d8a48a3bfe726f936863ad126d704
Braden Laverick <blav62@gmail.com>
2016-12-09 03:49:49 -0800

    Prototype Web Ui finished

--- Website/myapp/public/data.php
+++ Website/myapp/public/data.php
@@ -6,7 +6,6 @@ $servername = "localhost";
 $username = "sirenlocal";
 $password = "sirenproj";
 $db_name = "siren_db";
-print("helloworld");
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db_name);
 
@@ -17,40 +16,89 @@ if ($conn->connect_error) {
 
 
 //query to get data from the table
-$query = sprintf("SELECT username FROM AUTH;");
-
-//execute query
-$result = $conn->query($query);
-
-//loop through the returned data
+$queryuse = sprintf("SELECT username FROM AUTH;");
 $data = array();
-$uname_count = array();
-
-while( $row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-    $data[] = $row; // Inside while loop
+if ($result=mysqli_query($conn,$queryuse))
+    {
+  // Fetch one and one row
+    while ($row=mysqli_fetch_row($result)){
+        $data[] = $row[0];
+    }
+  // Free result set
+    mysqli_free_result($result);
 }
 
-foreach($data as $name){
-    printf ("%s\n",$name["username"]);
-    if (in_array($name.username, $uname_count, TRUE)){
-        //$uname_count[$name.username] ++;
-        printf ("TRUE\n");
+$querypass = sprintf("SELECT passwd FROM AUTH;");
+$datapass = array();
+if ($result=mysqli_query($conn,$querypass))
+    {
+  // Fetch one and one row
+    while ($row=mysqli_fetch_row($result)){
+        $datapass[] = $row[0];
     }
-    elseif(!in_array($name.username, $uname_count, TRUE)){
-    printf ("FALSE\n");
-    $uname_count[] = $name;
-    printf ("added %s to list\n", $uname_count["username"]);
-    }
+  // Free result set
+    mysqli_free_result($result);
 }
 
+$querysucc = sprintf("SELECT success FROM AUTH;");
+$datasucc = array();
+if ($result=mysqli_query($conn,$querysucc))
+    {
+  // Fetch one and one row
+    while ($row=mysqli_fetch_row($result)){
+        $datasucc[] = $row[0];
+    }
+  // Free result set
+    mysqli_free_result($result);
+}
+
+$queryip = sprintf("SELECT ip FROM SESSION;");
+$dataip = array();
+if ($result=mysqli_query($conn,$queryip))
+    {
+  // Fetch one and one row
+    while ($row=mysqli_fetch_row($result)){
+        $dataip[] = $row[0];
+    }
+  // Free result set
+    mysqli_free_result($result);
+}
+
+$querycmd = sprintf("SELECT input FROM INPUT;");
+$datacmd = array();
+if ($result=mysqli_query($conn,$querycmd))
+    {
+  // Fetch one and one row
+    while ($row=mysqli_fetch_row($result)){
+        $datacmd[] = $row[0];
+    }
+  // Free result set
+    mysqli_free_result($result);
+}
+
+//$querypass = sprintf("SELECT passwd FROM AUTH;");
+
+//$resultpass = $conn->query($querypass);
+
+$usernames = json_encode($data);
+$passwords = json_encode($datapass);
+$successes = json_encode($datasucc);
+$ips =  json_encode($dataip);
+$cmds = json_encode($datacmd);
+
+$dataout = array();
+$dataout[0] = $usernames;
+$dataout[1] = $passwords;
+$dataout[2] = $successes;
+$dataout[3] = $ips;
+$dataout[4] = $cmds;
+
+echo json_encode($dataout);
 
 //free memory associated with result
-mysqli_free_result($result);
 
 //close connection
 $conn->close();
 
 //now print the data
-print json_encode($data);
-
 ?>
--- Website/myapp/public/js/app.js
+++ Website/myapp/public/js/app.js
@@ -1,40 +1,179 @@
 $(document).ready(function(){
 	$.ajax({
-		url: "http://localhost:1234/data.php",
-		method: "GET",
+        type: "GET",
+		url: "data.php",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
 		success: function(data) {
-			console.log(data);
-			var player = [];
-			var score = [];
+			//$("#test").html(data);
+            console.log(data);
+            var usernames = JSON.parse(data[0]);
+            var passwords = JSON.parse(data[1]);
+            var successes = JSON.parse(data[2]);
+            var ips = JSON.parse(data[3]);
+            var commands = JSON.parse(data[4]);
 
-			for(var i in data){
-				player.push("Player " + data[i].playerid);
-				score.push(data[i].score);
-			}
+            var ipcount = [];
+            var uniqueips = [];
+            var un = 0;
+            for(i = 0; i < ips.length; i++) {
+                if(uniqueips.length != 0) {
+                    for (j = 0; j < uniqueips.length; j++) {
+                        if (ips[i] == uniqueips[j]) {
+                            ipcount[j]++;
+                            un = 1;
+                        }
+                    }
+                }
+                if(un == 0){
+                    uniqueips.push(ips[i]);
+                    ipcount.push(1);
+                }
+                else{
+                    un = 0;
+                }
+            }
 
-			var chartdata = {
-				labels: player,
-				datasets : [
-					{
-						label: 'Player Score',
-						backgroundColor: 'rgba(200, 200, 200, 0.75)',
-						borderColor: 'rgba(200, 200, 200, 0.75)',
-						hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
-						hoverBorderColor: 'rgba(200, 200, 200, 1)',
-						data: score
-					}
-				]
-			};
+            console.log(uniqueips);
+            console.log(ipcount);
 
-			var ctx = $("#mycanvas");
 
-			var barGraph = new Chart(ctx, {
+			var options = {
+                scales:{
+                    yAxes:[{
+                        display: true,
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            };
+
+			var ctx = $("#ipcanvas");
+			new Chart(ctx, {
 				type: 'bar',
-				data: chartdata
+				data: {
+				        labels: uniqueips,
+				        datasets : [{
+                            label: 'IP Address Hits',
+                            backgroundColor: 'rgba(200, 200, 200, 0.75)',
+                            borderColor: 'rgba(200, 200, 200, 0.75)',
+                            data: ipcount
+                        }]
+			            },
+                options: options
 			});
+
+            var uscount = [];
+            var uniqueusers = [];
+            var unus = 0;
+            for(i = 0; i < usernames.length; i++) {
+                if(uniqueusers.length != 0) {
+                    for (j = 0; j < uniqueusers.length; j++) {
+                        if (usernames[i] == uniqueusers[j]) {
+                            uscount[j]++;
+                            unus = 1;
+                        }
+                    }
+                }
+                if(unus == 0){
+                    uniqueusers.push(usernames[i]);
+                    uscount.push(1);
+                }
+                else{
+                    unus = 0;
+                }
+            }
+
+            var userchart = $("#usernames");
+			new Chart(userchart, {
+				type: 'bar',
+				data: {
+				        labels: uniqueusers,
+				        datasets : [{
+                            label: 'Common Usernames',
+                            backgroundColor: 'rgba(200, 200, 200, 0.75)',
+                            borderColor: 'rgba(200, 200, 200, 0.75)',
+                            data: uscount
+                        }]
+			            },
+                options: options
+			});
+
+            var passcount = [];
+            var uniquepass = [];
+            var unpass = 0;
+            for(i = 0; i < passwords.length; i++) {
+                if(uniquepass.length != 0) {
+                    for (j = 0; j < uniquepass.length; j++) {
+                        if (passwords[i] == uniquepass[j]) {
+                            passcount[j]++;
+                            unpass = 1;
+                        }
+                    }
+                }
+                if(unpass == 0){
+                    uniquepass.push(passwords[i]);
+                    passcount.push(1);
+                }
+                else{
+                    unpass = 0;
+                }
+            }
+
+            var passchart = $("#passwords");
+			new Chart(passchart, {
+				type: 'bar',
+				data: {
+				        labels: uniquepass,
+				        datasets : [{
+                            label: 'Common Passwords',
+                            backgroundColor: 'rgba(200, 200, 200, 0.75)',
+                            borderColor: 'rgba(200, 200, 200, 0.75)',
+                            data: passcount
+                        }]
+			            },
+                options: options
+			});
+
+            var good = 0;
+            var bad = 0;
+            for(i = 0; i < successes.length; i++){
+                if(successes[i] == 1){
+                    good++;
+                }
+                else{
+                    bad++;
+                }
+            }
+
+            var goodper = good/(successes.length) * 100;
+            var badper = bad/(successes.length) * 100;
+
+
+            var successPie = $('#successes');
+            new Chart(successPie,{
+                type: 'doughnut',
+                data: {
+                    labels: ["Successful Entry", "Failed Entry"],
+                    datasets: [{
+                        backgroundColor: ['#811BD6', '#9CBABA'],
+                        data: [goodper, badper]
+                    }]
+                }
+            });
+
+            var cmdlist = "";
+            for(i = 0; i < commands.length; i++){
+                cmdlist = cmdlist + "<li>" + commands[i] + "</li>";
+            }
+            $('#input').html(cmdlist);
+
+
 		},
-		error: function(data) {
-			console.log(data);
+		error: function(ts) {
+            console.log("error");
+            console.log(ts.responseText);
 		}
 	});
 });
