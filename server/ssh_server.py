927222fb2f00dd6f1aea6c4aed87a24ce0e27702
Braden Laverick <blav62@gmail.com>
2017-01-28 09:47:59 -0700

    ssh server file added

---
+++ server/ssh_server.py
@@ -0,0 +1,9 @@
+#############################################################
+# NAME: C1C Braden J Laverick                               #
+# PROJECT: SIREN Project                                    #
+# FILE: SIREN Server Classes - telnet_server.py             #
+# DESCRIPTION:                                              #
+# This file specifies the classes for the various control   #
+# servers that SIREN implements.                            #
+#############################################################
+
