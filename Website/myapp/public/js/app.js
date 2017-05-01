05d195301907a0513b63029a72cd36137e16d18a
Braden Laverick <blav62@gmail.com>
2017-05-01 17:09:10 -0600

    Testing snort page

--- Website/myapp/public/js/app.js
+++ Website/myapp/public/js/app.js
@@ -383,7 +383,7 @@ $(document).ready(function(){
                     }
                 }
                 for(j=0; j<snortsigs.length; j++){
-                    if(snortevents[i][3] == snortsigs[j][0]){
+                    if(snortevents[i][3] == snortsigs[j][6]){
                         eventsigs.push(snortsigs[j][1])
                     }
                 }
